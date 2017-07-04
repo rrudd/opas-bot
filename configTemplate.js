1e58a7c2c19c76f3442cfe0cc693b1096f80089e
rrudd <raoul.udd@netlight.com>
2017-07-04 17:16:30 +0300

    update config template to contain geocoding endpoint uri

--- configTemplate.js
+++ configTemplate.js
@@ -1,6 +1,7 @@
 const config = {
   token: 'your_key_here',
   routingEndpoint: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+  geocodingEndpoint: 'http://api.digitransit.fi/geocoding/v1/search',
 }
 
 module.exports = config;
