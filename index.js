7870f5fca9063a08b845db92d57b3deff8ac7572
rrudd <raoul.udd@netlight.com>
2017-07-05 17:21:09 +0300

    allow usage of env vars for api token

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const telegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 const itinerary = require('./app/itinerary/itinerary')
 
-const bot = new telegramBot(config.token, {polling: true});
+const API_TOKEN = process.env.OPAS_BOT_TOKEN || config.token;
+
+const bot = new telegramBot(API_TOKEN, {polling: true});
 
 bot.onText(new RegExp(config.cmds.route), async function(msg, match) {
   bot.sendMessage(msg.from.id, await itinerary(msg), {parse_mode: 'Markdown'});
